f083aa6c502cab128678b0ed485cff3a80931e7b
Kapleen <bayturaev23@gmail.com>
2025-07-24 03:07:42 +0500

    Доработка сайта с HTML

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ def home():
     return "<h1>Добро пожаловать на сайт!</h1>"
 
 @app.route("/products")
-def products():
-    data = [
+def get_products():
+    products = [
         {"name": "Крем для лица", "price": 80000},
         {"name": "Шампунь", "price": 50000},
         {"name": "Помада", "price": 65000}
     ]
-    return jsonify(data)
+    return render_template('products.html', products=products)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
